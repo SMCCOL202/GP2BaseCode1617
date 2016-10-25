38bd7f1914ab083869ece6c0a2eda8e37acd6cba
SMCCOL202 <smccol202@caledonian.ac.uk>
2016-10-25 14:58:48 +0100

    Lab5 finished Almost

---
+++ GameApplication/include/GameObject.h
@@ -0,0 +1,55 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "Common.h"
+#include "Texture.h"
+#include "Shader.h"
+#include "Vertex.h"
+
+//TODO: Need to add fucntions to change position, rotation, and scale.
+
+class GameObject
+{
+public:
+	GameObject();
+	~GameObject();
+
+	void onRender(mat4& view, mat4& projection);
+
+	void onUpdate();
+
+	void onInit();
+
+	void onDestroy();
+
+	void loadTexture(const string& filename);
+
+	void loadShaders(const string& vsFilename, const string& fsFilename);
+
+	void copyVertexData(Vertex* pVerts, int numberOfVertices);
+
+private:
+	GLuint m_VBO;
+	GLuint m_VAO;
+
+	GLuint m_ShaderProgram;
+
+	mat4 m_ModelMatrix;
+
+	GLuint m_Texture;
+	GLuint m_ClampSampler;
+
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+	mat4 m_TranslationMatrix;
+	mat4 m_RotationMatrix;
+	mat4 m_ScaleMatrix;
+
+	int m_NumberOfVertices;
+
+protected:
+};
+
+#endif
